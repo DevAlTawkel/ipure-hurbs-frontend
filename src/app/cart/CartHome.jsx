b5d90e4115ec92107a24bfd01debfae1f5bc3945
AseemTawkel <aseem@altawkelcenter.com>
2026-06-08 21:37:28 +0530

    Added the Add to cart page

---
+++ src/app/cart/CartHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CartContent from './CartContent'
+
+const CartHome = () => {
+    return (
+        <>
+            <CartContent />
+        </>
+    )
+}
+
+export default CartHome
