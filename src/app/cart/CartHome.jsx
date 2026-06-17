9812798daa4e3de3795fa7e10a1930b664f197b2
AseemTawkel <aseem@altawkelcenter.com>
2026-06-17 16:32:03 +0530

    Add meta tag - facebook

--- src/app/cart/CartHome.jsx
+++ src/app/cart/CartHome.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import CartContent from './CartContent'
+import TopSlider from '@/components/TopSlider'
 
 const CartHome = () => {
     return (
         <div className='webpage-container'>
+            <TopSlider
+                slides={[
+                    'Great choices deserve a smooth checkout!',
+                    'You’re one step closer to better well-being!'
+                ]}
+            />
             <CartContent />
         </div>
     )
